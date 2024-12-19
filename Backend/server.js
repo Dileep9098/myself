40f40db7ec1109bc7dbcf1ab8f23c284f84d8e43
Dileep Sahu <118627904+Dileep9098@users.noreply.github.com>
2024-12-19 18:03:48 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -71,13 +71,7 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors(
-  {
-   origin:["https://myself-git-main-dileeps-projects-853f5e81.vercel.app/"],
-    methods:["POST",GET"],
-    credentials:true
-}
-));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
